0fbb9acf2b00118084eeff74889944420360796f
acosbraunaportapaletes-jpg <acosbraunaportapaletes@gmail.com>
2025-08-12 22:51:59 -0300

    Add files via upload

---
+++ package.json.js
@@ -0,0 +1,15 @@
+{
+  "name": "meu-bot-telegram",
+  "version": "1.0.0",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js",
+    "webhook": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.x",
+    "dotenv": "^16.x",
+    "mercadopago": "^2.x",
+    "node-telegram-bot-api": "^0.61.0"
+  }
+}
